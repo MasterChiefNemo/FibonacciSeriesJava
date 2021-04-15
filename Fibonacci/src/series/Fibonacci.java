e7ef47febe92c4601c0638368ac301328da783e0
Damien Hunter <a00014290@student.ait.ie>
2021-04-15 11:47:12 +0100

    modified:   Fibonacci/bin/series/Fibonacci.class 	modified:   Fibonacci/src/series/Fibonacci.java 	Fibonacci/bin/.gitignore

--- Fibonacci/src/series/Fibonacci.java
+++ Fibonacci/src/series/Fibonacci.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 
 public class Fibonacci{
 	public static ArrayList<Integer> fibSeries(int limit){
-		ArrayList<Integer> list = new ArrayList<Integer>();
+		ArrayList<Integer> list = new ArrayList<Integer>(); // Create an Integer ArrayList and add the 1st 2 elements required.
 		list.add(0);
 		list.add(1);
 		
-		for(int x = 1; x < limit; x++){
+		for(int x = 1; x < limit; x++){ // User defined upper limit determines when to stop.
 			int add = list.get(x) + list.get(x - 1);
 			list.add(add);
 		}
@@ -16,6 +16,7 @@ public class Fibonacci{
 	}
 
 	public static void main(String[] args){
+		// This is purely to test the method with a limit.
 		ArrayList<Integer> newList = Fibonacci.fibSeries(30);
 		
 		for(int x = 0; x < newList.size(); x++){
